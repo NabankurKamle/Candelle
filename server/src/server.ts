9ea8f216cf301816e6129c63067c58bb9461eea7
Nabankur Kamle <nabankurkamle@gmail.com>
2026-05-12 15:30:34 +0530

    Initial commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,8 +11,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "*" }));
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        process.env.FRONTEND_URL!,
+    ],
+    credentials: true,
+}));
 app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({
+    extended: true,
+    limit: '20mb',
+}));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.use('/api/queues', queueRoutes);
